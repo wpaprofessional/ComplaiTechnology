0d484cdbca8a935f617a1e90fb5bb0de6d46a12d
Wesley de Paula Almeida <wesleydepaulaalmeida@hotmail.com>
2024-07-14 01:27:18 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Projeto Semana de ADM
+
+Bem-vindo ao meu primeiro projeto utilizando HTML e CSS! Este projeto foi criado como parte de uma atividade escolar chamada "Semana de ADM", onde criamos uma empresa fictícia e a apresentamos para as turmas da escola.
+
+## Sobre o Projeto
+
+Este projeto foi desenvolvido com o objetivo de criar um site para nossa empresa fictícia. Todo o conteúdo e as ideias foram inspirados e guiados pelo canal do YouTube Inteliogia.
+
+## Recursos Utilizados
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+## Agradecimentos
+
+Gostaria de agradecer ao canal [Inteliogia](https://youtube.com/@InteliogiaDev?si=wvdcDQasUtdz0PzU) por fornecer conteúdo valioso que ajudou no meu desenvolvimento como desenvolvedor web.
+
+## Observações
+
+Este é meu primeiro projeto e estou fazendo tudo sozinho, utilizando o máximo de conteúdo disponível na internet. Não espero que seja perfeito, mas estou focado no aprendizado que ele proporciona.
+
+## Como Visualizar o Projeto
+
+Para visualizar o projeto, basta clonar este repositório e abrir o arquivo `index.html` no seu navegador.
+
+```bash
+git clone https://github.com/seu-usuario/nome-do-repositorio.git
+cd nome-do-repositorio
+open index.html
---
+++ reveal.js
@@ -0,0 +1,221 @@
+window.revelar = ScrollReveal({ reset: true })
+//INDEX.HTML
+// TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',
+    {
+        duration: 1000,
+        distance: '90px'
+    })
+
+revelar.reveal('.efeito-img-topo',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+// OS TITULOS DA SESSOES
+revelar.reveal('.titulo-edit',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+//SESSAO DE ESPECIALIDADES
+revelar.reveal('.efeito-especialidades1',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-especialidades2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-especialidades3',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-especialidades4',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-especialidades5',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+revelar.reveal('.efeito-especialidades6',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+//EFEITO PORTIFOLIO
+revelar.reveal('.efeito-portifolio1',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'bottom'
+    })
+
+revelar.reveal('.efeito-portifolio2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'top'
+    })
+
+revelar.reveal('.efeito-portifolio3',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'bottom'
+    })
+
+// EFEITO FORMULARIO
+revelar.reveal('.efeito-formulario1',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-formulario2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-formulario3',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-formulario4',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-formulario-btn',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500
+    })
+
+
+//SERVIÇOS.HTML
+//TOPO DO SITE
+revelar.reveal('.efeito-txt-servicos',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-img-servicos',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+//COOPERADORES.HTML
+
+
+//SOBRE.HTML
+//EFEITO HISTORIA
+revelar.reveal('.efeito-historia-txt',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-historia-img',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+//EFEITO ORIENTACAO
+revelar.reveal('.efeito-orientacao1',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-orientacao2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-orientacao3',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'left'
+    })
+
+revelar.reveal('.efeito-orientacao-txt1',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-orientacao-txt2',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
+revelar.reveal('.efeito-orientacao-txt3',
+    {
+        duration: 1000,
+        distance: '90px',
+        delay: 500,
+        origin: 'right'
+    })
+
